d8cea5390e0205e6340a28895648459adc591c90
Riwert <riwert@gmail.com>
2023-06-28 12:11:50 +0200

    change product name tag to h2 for higher priority

--- src/components/ShopwareProduct.vue
+++ src/components/ShopwareProduct.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 <template>
   <div class="card bg-white shadow h-100">
     <div class="card-body d-flex flex-column justify-content-between">
-      <h3 class="card-title h6 fw-bold">{{ product.translated.name || product.name }}</h3>
+      <h2 class="card-title h6 fw-bold">{{ product.translated.name || product.name }}</h2>
       <p class="card-text">{{ product.translated.description || product.description }}</p>
       <p class="card-text"><strong>Cena</strong> {{ usePriceFormatter(product.calculatedPrice.totalPrice) }} zł</p>
     </div>
